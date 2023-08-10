5344192f0f1185c817c16c174cd265fcdd24222b
alexkasema <lexxmulwa@gmail.com>
2023-08-10 06:33:36 -0700

    <string.h> (+1)A C Program that prints a string, followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,6 @@ int main(void)
 {
 	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	write(STDOUT_FILENO, s, strlen(s));
+	write(STDOUT_FILENO, s, strlen(s) + 1);
 	return (1);
 }
