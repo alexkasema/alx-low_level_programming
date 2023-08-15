8d46d968fd5807d198f08e944a3fc0d5198ba8c4
alexkasema <lexxmulwa@gmail.com>
2023-08-15 08:31:10 -0700

    A program that prints out a word using another function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *
+ *Return: 0 Always (Success)
+ */
+int main(void)
+{
+	char w[8] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 8; i++)
+		_putchar(w[i]);
+	_putchar('\n');
+
+	return (0);
+}
