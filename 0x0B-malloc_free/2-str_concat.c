235ebbe0831e5095ae8630e975a9d2a9ee8e37b6
alexkasema <lexxmulwa@gmail.com>
2023-09-04 06:05:46 -0700

    A function that concatenates two strings.....

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,39 +7,46 @@
  *
  *Return: Pointer to newly allocated string
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	int i, len1 = 0, len2 = 0;
+	int i = 0, len1 = 0, len2 = 0;
+	char *ns, *ss1, *ss2;
 
-	char *ns;
-
+	ss1 = s1;
+	ss2 = s2;
 	if (s1 == NULL)
-		s1 == "";
-
-	if (s2 == NULL)
-		s2 == "";
-
-	for (i = 0; s1[i]; i++)
+		s1 = "";
+	while (*s1)
+	{
 		len1++;
-
-	for (i = 0; s2[i]; i++)
-		len2++;
-
+		s1++;
+	}
+	s1 = ss1;
+	if (s2 == NULL)
+		s2 = "";
+	while (*s2)
+	{
+		lens++;
+		s2++;
+	}
+	s2 = ss2;
 	ns = malloc(sizeof(char) * (len1 + len2 + 1));
-
+	ss1 = ns;
 	if (ns == NULL)
 		return (NULL);
-
-	for (i = 0; s1[i]; i++)
-		ns[i] = s1[i];
-
-	for (i = 1; s2[i]; i++)
-		ns[len1 + i] = s2[i];
-
-	ns[len1 + len2] = '\0';
-
-	return (ns);
-
+	for (; i < (len1 + len2); i++)
+	{
+		if (i < len1)
+		{
+			ns[i] = *s1;
+			s1++;
+		}
+		else
+		{
+			ns[i] = *s2;
+			s2++;
+		}
+	}
+	ns[i] = '\0';
+	return (ss1);
 }
-
