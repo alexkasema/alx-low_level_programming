ce1963b3d3c594b9a568ff16b6062cbccf7eeeb6
alexkasema <lexxmulwa@gmail.com>
2023-09-11 06:12:18 -0700

    Defining a new type struct dog with some elements.

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+# Structures, typedef in C
+## dog.h
+Define a new type struct dog with the following elements char *name
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - New type dog
+ *@name: The name of the dog
+ *@age: Age of the dog
+ *@owner: Owner of the dog
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif /* Struct dog*/
+
