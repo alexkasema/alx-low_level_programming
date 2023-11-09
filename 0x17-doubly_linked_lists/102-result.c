7b3601521130549260786a0c42a3b40772eb67ce
alexkasema <lexxmulwa@gmail.com>
2023-11-09 06:26:56 -0800

    the largest palindrome made from the product of two 3-digit numbers.

---
+++ 0x17-doubly_linked_lists/102-result.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * main - Find the largest palindrome made from the product of
+ *	two 3-digit numbers.
+ *
+ * Return: (0)Success else 1
+ *
+ */
+
+int main(void)
+{
+	int i, j, num = 0, largest = 0;
+
+	for (i = 999; i >= 100; i--)
+	{
+		for (j = 999; j >= 100; j--)
+		{
+			num = i * j;
+			if (is_palindrome(num))
+			{
+				if (num > largest)
+					largest = num;
+			}
+		}
+	}
+	printf("%d\n", largest);
+
+	return (0);
+}
+
+/**
+ * is_palindrome - A function that reverses a number and checks its same
+ * @num: Number to check
+ * Return: 1 if same
+ */
+
+int is_palindrome(int num)
+{
+	int n = num, rev = 0;
+	while (n)
+	{
+		rev = rev * 10 + n % 10;
+		n /= 10;
+	}
+
+	return (rev == num);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -33,6 +33,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 void free_dlistint(dlistint_t *head);
 
+int is_palindrome(int num);
+
 
 #endif
 
